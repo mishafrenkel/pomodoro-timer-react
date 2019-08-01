cca31477a890fa31b969d77b77392bc2c6bab62d
Michael Frenkel <michael.frenkel@outlook.com>
2019-07-31 23:05:38 -0700

    initialize state in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,23 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor(props) {
     super(props);
-
-    
+    this.state = {
+      workLength: JSON.parse(localStorage.getItem('workTimer')) || 25,
+      breakLength: JSON.parse(localStorage.getItem('breakTimer')) || 5,
+      currentMinutes: JSON.parse(localStorage.getItem('workTimer')) || 25,
+      currentSeconds: 0,
+      currentSession: 'Work',
+      currentButton: {
+        func: () => this.countDown(),
+        icon: 'fa-play',
+        title: 'Play'
+      },
+      modalStyle: { display: 'none' },
+      errorStyle: { display: 'none' }
+    };
+    this.timer = null;
+    this.workTimer = this.state.workLength * 60;
+    this.breakTimer = this.state.breakLength * 60;
+    this.playTimer = this.playTimer.bind(this);
   }
 }
